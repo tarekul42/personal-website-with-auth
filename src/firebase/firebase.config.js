724b51737de267aa544f3fb14fd77680fc6a4f17
tarekul42 <tarekulrifat205@gmail.com>
2023-09-27 15:14:45 -0700

    envioronment setup for this project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeiysprc5hPPJFKyZm8ITDDRPHJsKnDPA",
+  authDomain: "personal-website-with-auth.firebaseapp.com",
+  projectId: "personal-website-with-auth",
+  storageBucket: "personal-website-with-auth.appspot.com",
+  messagingSenderId: "585509307904",
+  appId: "1:585509307904:web:581be43d93afa8c08efae5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-3xl underline">
+    Hello world!
+  </h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
